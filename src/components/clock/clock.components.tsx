40e0d8d92beaf97221f748e30f755eb70347675d
Balabounce <yoba.number.one@gmail.com>
2021-09-27 18:32:11 +0300

    complete

--- src/components/clock/clock.components.tsx
+++ src/components/clock/clock.components.tsx
@@ -1,94 +1,65 @@
-import React, { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
 import './clock.style.scss';
 import arrowUp from "../../assets/arrow-up-solid.svg";
 import arrowDown from "../../assets/arrow-down-solid.svg";
 import playBtn from '../../assets/play-solid.svg';
 import pauseBtn from '../../assets/pause-solid.svg';
 import reloadBtn from '../../assets/sync-solid.svg';
+// import {audio1} from '../../assets/papich.mp3';
 
 const Clock = () => {
-    let [mode, setMode] = useState('Session');
+    let [power,setPower] = useState(false);
+    let [mode, setMode] = useState(true);  //true = session, false = break
     let [sessionTime, setSessionTime] = useState(25);
     let [breakTime, setBreakTime] = useState(5);
     let [timeInBox, setTimeInBox] = useState('25:00');
-    let [timerId, setTimerId] = useState(
-        setInterval(() => 
-            { return;
-        }, 5000))
+    let [timerId, setTimerId] = useState(setTimeout(() => { return;}, 5000))
+    
 
+    useEffect(() => {
+        setTimeInBox(`${sessionTime}:00`);
+    }, [sessionTime]);
 
-    const updateTimeInBox = () => {
-        if(mode === 'Session') {
+    useEffect(() => {
+        if(power) {
+            if(timeInBox === '0:00') {
+                const audio = new Audio('papich.mp3');
+                audio.play();
+                setMode(!mode);
+            }
+            const timer = timeInBox.split(':');
+            let time = new Date();
+            time.setMinutes(+timer[0]);
+            time.setSeconds(+timer[1]);
+            setTimerId(setTimeout(() => {
+                time.setSeconds(time.getSeconds() - 1);
+                if(time.getSeconds() < 10) {
+                    setTimeInBox(`${time.getMinutes()}:0${time.getSeconds()}`);
+                } else setTimeInBox(`${time.getMinutes()}:${time.getSeconds()}`);
+            }, 1000));
+        } else {
+            clearTimeout(timerId);
+        }
+    }, [power, timeInBox]);
+
+    useEffect(() => {
+        clearTimeout(timerId);
+        if(mode === false) {
+            setTimeInBox(`${breakTime}:00`);
+        } else {
             setTimeInBox(`${sessionTime}:00`);
         }
-        if(mode === 'Break') {
-            setTimeInBox(`${breakTime}:00`);
-        }
-    }
-
-    const changeTime = (oper: string, box: string) => {
-        if(box === 'session') {
-            if(oper === 'minus') {
-                setSessionTime(--sessionTime);
-            }
-            if(oper === 'plus') {
-                setSessionTime(++sessionTime);
-            }
-        }
-        if(box === 'break') {
-            if(oper === 'minus') {
-                setBreakTime(--breakTime);
-            }
-            if(oper === 'plus') {
-                setBreakTime(++breakTime);
-            }
-        }
-        updateTimeInBox();        
-    }
-
-    const changeMode = () => {
-        console.log(mode);
-        if(mode === 'Session') {
-            setMode('Break');
-        } else {
-            setMode('Session');
-        }
-        updateTimeInBox();
-    }
-
-    const timerActivating = () => {
-        let time = timeInBox.split(':');
-        let minutes = +time[0];
-        let seconds = +time[1];
-        let timer = new Date();
-        timer.setMinutes(minutes, seconds);
-        setTimerId(
-            setInterval(() => {
-                timer.setSeconds(timer.getSeconds() - 1);
-                console.log(timerId);
-                if(timer.getSeconds() === 58 && timer.getMinutes() === 0) {
-                    clearInterval(timerId);
-                    changeMode();
-                    timerActivating();
-                }
-                if(timer.getSeconds() < 10) {
-                    setTimeInBox(`${timer.getMinutes()}:0${timer.getSeconds()}`);
-                } else {
-                    setTimeInBox(`${timer.getMinutes()}:${timer.getSeconds()}`);
-                }
-            }, 1000)
-        );
-    }
-
-    const pause = () => {
-        clearInterval(timerId);
-    }
+    }, [mode]);
 
     const reload = () => {
-        clearInterval(timerId);
+        clearTimeout(timerId);
+        setPower(false);
+        setMode(true);  //true = session, false = break
         setSessionTime(25);
         setBreakTime(5);
         setTimeInBox('25:00');
+        setTimerId(setTimeout(() => { return;}, 5000));
     }
 
     return (
@@ -97,19 +68,19 @@ const Clock = () => {
                 <div className="title">25 + 5 Clock</div>
                 <div className="length">
                     <div className='break'>
-                        <p>Break Length</p>
+                        <p id='break-label'>Break Length</p>
                         <div className="arrows">
-                            <button className="arrow-up" onClick={() => {
+                            <button className="arrow-up"  id="break-increment" onClick={() => {
                                 if(breakTime < 60) {
-                                    changeTime('plus', 'break');
+                                    setBreakTime(breakTime + 1)
                                 }
                             }}>
                                 <img src={arrowUp} alt="up" />
                             </button>
-                            <p>{breakTime}</p>
-                            <button className="arrow-down" onClick={() => {
+                            <p id="break-length">{breakTime}</p>
+                            <button className="arrow-down" id="break-decrement" onClick={() => {
                                 if(breakTime > 1) {
-                                    changeTime('minus', 'break');
+                                    setBreakTime(breakTime - 1)
                                 }
                             }}>
                                 <img src={arrowDown} alt="down" />
@@ -117,19 +88,18 @@ const Clock = () => {
                         </div>
                     </div>
                     <div className='session'>
-                        <p>Session Length</p>
+                        <p id="session-label">Session Length</p>
                         <div className="arrows">
-                            <button className="arrow-up" onClick={() => {
+                            <button className="arrow-up" id="session-increment" onClick={() => {
                                 if(sessionTime < 60) {
-                                    changeTime('plus', 'session')
-                                }
+                                    setSessionTime(sessionTime + 1)                                }
                             }}>
                                 <img src={arrowUp} alt="up" />
                             </button>
-                            <p>{sessionTime}</p>
-                            <button className="arrow-down" onClick={() => {
+                            <p id="session-length">{sessionTime}</p>
+                            <button className="arrow-down" id="session-decrement" onClick={() => {
                                 if(sessionTime > 1) {
-                                    changeTime('minus', 'session')
+                                    setSessionTime(sessionTime - 1)                                
                                 }
                             }}>
                                 <img src={arrowDown} alt="down" />
@@ -138,21 +108,24 @@ const Clock = () => {
                     </div>
                 </div>
                 <div className="timer-box">
-                    <p className="timer-box-title">{mode}</p>
-                    <p className="timer">{timeInBox}</p>
+                    <p className="timer-box-title" id="timer-label">{mode ? 'Session' : 'Break'}</p>
+                    <p className="timer" id="time-left" >{timeInBox}</p>
                 </div>
                 <div className="timer-functions">
-                    <button onClick={() => timerActivating()}>
-                        <img src={playBtn} alt="play" />
-                    </button>
-                    <button onClick={() => pause()}>
-                        <img src={pauseBtn} alt="pause" />
-                    </button>
-                    <button onClick={() => reload()}>
+                    <div id="start_stop">
+                        <button onClick={() => {
+                            setPower(!power);
+                        }}>
+                            <img src={playBtn} alt="play" />
+                            <img src={pauseBtn} alt="pause" />
+                        </button>
+                    </div>
+                    <button id="reset" onClick={() => reload()}>
                         <img src={reloadBtn} alt="reload" />
                     </button>
                 </div>
             </div>
+            <audio id="beep" src="../../assets/papich.mp3"></audio>
         </section>
     )
 }
